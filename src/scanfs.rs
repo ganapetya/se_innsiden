7796adb0f18de3e88a1148da71eb75aedbbac176
@Peter Kovgan <peter.kovgan@gmail.com>
2024-10-16 12:37:57 +0300

    avoid unwrap on questionable results

--- src/scanfs.rs
+++ src/scanfs.rs
@@ -4,13 +4,16 @@ use std::fs::{self, DirEntry};
 pub fn scan_directory(dir_path: Option<&String>) -> HashMap<String, DirEntry> {
     let mut fs_elements = HashMap::new();
     if let Some(path) = dir_path {
-        let paths = fs::read_dir(path).unwrap();
-        for one_path in paths {
-            println!("Name: {}", one_path.as_ref().unwrap().path().display());
-            let element = one_path.unwrap();
-            let owned_element: Option<String> = element.path().to_str().map(str::to_string);
-            if let Some(element_as_string) = owned_element {
-                fs_elements.insert(element_as_string, element);
+        let result_paths = fs::read_dir(path);
+        if let Ok(present_paths) = result_paths {
+            for result_one_path in present_paths {
+                if let Ok(one_path_present) = result_one_path {
+                    println!("Name: {}", one_path_present.path().display());
+                    let owned_element: Option<String> = one_path_present.path().to_str().map(str::to_string);
+                    if let Some(element_as_string) = owned_element {
+                        fs_elements.insert(element_as_string, one_path_present);
+                    }
+                }
             }
         }
     }
