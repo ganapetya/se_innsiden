24f97b13252bd131f432205a766633d4a985e668
@Peter Kovgan <peter.kovgan@gmail.com>
2024-10-17 20:09:25 +0300

    file size display

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use scanfs::scan_directory;
 use std::env;
+//windows extention
+use std::os::windows::fs::MetadataExt;
+use std::fs;
 
 mod scanfs;
 
@@ -9,6 +12,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let found = scan_directory(args.get(PATH_PARAMETER));
     for path in found {
-        println!("Map Entry: {:?}", path);
+        let path_name = path.0;
+        let metadata = fs::metadata(&path_name);
+        if let Ok(metadata) = metadata {
+            let file_size = metadata.file_size();
+            println!("Map Entry [path: {:?} size: {:?}]", &path_name, file_size);
+        }
     }
 }
