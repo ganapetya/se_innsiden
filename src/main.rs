e75371d223dc2b3b3d5b6be5b4833d31cc6fa1af
@Peter Kovgan <peter.kovgan@gmail.com>
2024-10-18 21:06:35 +0300

    calculate dir/file size

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use scanfs::scan_directory;
 use std::env;
-//windows extention
-use std::os::windows::fs::MetadataExt;
-use std::fs;
 
 mod scanfs;
 
@@ -11,12 +8,8 @@ const PATH_PARAMETER: usize = 1;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let found = scan_directory(args.get(PATH_PARAMETER));
+
     for path in found {
-        let path_name = path.0;
-        let metadata = fs::metadata(&path_name);
-        if let Ok(metadata) = metadata {
-            let file_size = metadata.file_size();
-            println!("Map Entry [path: {:?} size: {:?}]", &path_name, file_size);
-        }
+        println!("Map Entry [path: {:?} ", path);
     }
 }
--- src/scanfs.rs
+++ src/scanfs.rs
@@ -1,47 +1,64 @@
+use log::{info, warn};
 use rayon::iter::ParallelBridge;
 use rayon::prelude::ParallelIterator;
-use std::collections::HashMap;
-use std::fs::{self, DirEntry, ReadDir};
+use std::fs::{self, ReadDir};
+use std::os::windows::fs::MetadataExt;
 use std::sync::mpsc::{channel, Sender};
-use log::{info, warn};
 
-pub fn scan_directory(dir_path: Option<&String>) -> HashMap<String, DirEntry> {
+#[derive(Debug)]
+pub enum FsElementType {
+    FILE,
+    DIR,
+}
+
+#[derive(Debug)]
+pub struct FsElementInfo {
+    level: u32,
+    path: String,
+    size_bytes: u64,
+    kind: FsElementType,
+}
+
+pub fn scan_directory(dir_path: Option<&String>) -> Vec<FsElementInfo> {
     let reciever = {
         let (sender, reciever) = channel();
 
-        scan_directory_req(dir_path, &sender);
+        scan_directory_req(dir_path, &sender, 0);
 
         reciever
     };
-
-    let all_paths: Vec<DirEntry> = reciever.into_iter().par_bridge().collect();
-
-    let mut fs_elements = HashMap::new();
-
-    for file_system_element in all_paths {
-        let owned_element: Option<String> = file_system_element.path().to_str().map(str::to_string);
-        if let Some(element_as_string) = owned_element {
-            fs_elements.insert(element_as_string, file_system_element);
-        }
-    }
-
-    fs_elements
+    reciever.into_iter().par_bridge().collect()
 }
 
-fn scan_directory_req(dir_path: Option<&String>, sender: &Sender<DirEntry>) -> () {
+fn scan_directory_req(
+    dir_path: Option<&String>,
+    sender: &Sender<FsElementInfo>,
+    level: u32,
+) -> u64 {
     if let Some(path) = dir_path {
         let read_dir_result = fs::read_dir(path);
-        match read_dir_result {
+        return match read_dir_result {
             Ok(present_paths) => {
                 info!("Collecting data from directory {}", path);
-                collect_directory_data(present_paths, sender)
-            },
-            Err(error) => warn!("Can not read directory {path} : {error}"),
+                collect_directory_data(present_paths, sender, level, path)
+            }
+            Err(error) => {
+                warn!("Can not read directory {path} : {error}");
+                0
+            }
         };
     }
+    0
 }
 
-fn collect_directory_data(present_paths: ReadDir, sender: &Sender<DirEntry>) -> () {
+fn collect_directory_data(
+    present_paths: ReadDir,
+    sender: &Sender<FsElementInfo>,
+    level: u32,
+    parent_path: &String,
+) -> u64 {
+    let mut result: u64 = 0;
+
     present_paths.for_each(|result_one_path| {
         if let Ok(one_path_present) = result_one_path {
             let path_name = one_path_present.path();
@@ -49,15 +66,32 @@ fn collect_directory_data(present_paths: ReadDir, sender: &Sender<DirEntry>) -> 
                 if file_type.is_dir() {
                     let path_param: Option<String> =
                         one_path_present.path().to_str().map(|e| e.to_string());
-                    scan_directory_req(path_param.as_ref(), sender);
-                }
-                match sender.send(one_path_present) {
-                    Ok(_) => {
-                        info!("Successfully sent into the queue file info {:?}", &path_name);            
-                    },
-                    Err(error) => warn!("Can not send into queue fine info {:?} : {error}", &path_name),
+                    result += scan_directory_req(path_param.as_ref(), sender, level + 1);
+                } else if file_type.is_file() {
+                    let metadata = fs::metadata(&path_name);
+                    if let Ok(metadata) = metadata {
+                        let file_size = metadata.file_size();
+                        result += file_size;
+                        let file_info = FsElementInfo {
+                            level: level + 1,
+                            path: path_name.display().to_string(),
+                            size_bytes: file_size,
+                            kind: FsElementType::FILE,
+                        };
+                        let _ = sender.send(file_info);
+                    }
                 }
             }
         }
     });
+
+    let file_info = FsElementInfo {
+        level: level,
+        path: parent_path.to_string(),
+        size_bytes: result,
+        kind: FsElementType::DIR,
+    };
+    let _ = sender.send(file_info);
+
+    result
 }
