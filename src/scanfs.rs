b804d4953edb9f5385ff0a2360b52d14de2d73ce
@Peter Kovgan <peter.kovgan@gmail.com>
2024-10-18 19:36:40 +0300

    refactoring, logs, errors processing

--- src/scanfs.rs
+++ src/scanfs.rs
@@ -3,12 +3,13 @@ use rayon::prelude::ParallelIterator;
 use std::collections::HashMap;
 use std::fs::{self, DirEntry, ReadDir};
 use std::sync::mpsc::{channel, Sender};
+use log::{info, warn};
 
 pub fn scan_directory(dir_path: Option<&String>) -> HashMap<String, DirEntry> {
     let reciever = {
         let (sender, reciever) = channel();
 
-        scan_directory_send(dir_path, &sender);
+        scan_directory_req(dir_path, &sender);
 
         reciever
     };
@@ -27,26 +28,35 @@ pub fn scan_directory(dir_path: Option<&String>) -> HashMap<String, DirEntry> {
     fs_elements
 }
 
-fn scan_directory_send(dir_path: Option<&String>, sender: &Sender<DirEntry>) -> () {
+fn scan_directory_req(dir_path: Option<&String>, sender: &Sender<DirEntry>) -> () {
     if let Some(path) = dir_path {
-        let result_paths = fs::read_dir(path);
-        if let Ok(present_paths) = result_paths {
-            collect_fs_data(present_paths, sender);
-        }
+        let read_dir_result = fs::read_dir(path);
+        match read_dir_result {
+            Ok(present_paths) => {
+                info!("Collecting data from directory {}", path);
+                collect_directory_data(present_paths, sender)
+            },
+            Err(error) => warn!("Can not read directory {path} : {error}"),
+        };
     }
 }
 
-fn collect_fs_data(present_paths: ReadDir, sender: &Sender<DirEntry>) -> () {
+fn collect_directory_data(present_paths: ReadDir, sender: &Sender<DirEntry>) -> () {
     present_paths.for_each(|result_one_path| {
         if let Ok(one_path_present) = result_one_path {
+            let path_name = one_path_present.path();
             if let Ok(file_type) = one_path_present.file_type() {
                 if file_type.is_dir() {
                     let path_param: Option<String> =
                         one_path_present.path().to_str().map(|e| e.to_string());
-                    scan_directory_send(path_param.as_ref(), sender);
+                    scan_directory_req(path_param.as_ref(), sender);
                 }
-                //TODO: process or note negative result
-                let _ = sender.send(one_path_present);
+                match sender.send(one_path_present) {
+                    Ok(_) => {
+                        info!("Successfully sent into the queue file info {:?}", &path_name);            
+                    },
+                    Err(error) => warn!("Can not send into queue fine info {:?} : {error}", &path_name),
+                }
             }
         }
     });
